1927d5a8047d8b9e4f095865d4706dc642675077
Rutuja Deshmukh <66289022+develop-er11@users.noreply.github.com>
2022-06-22 18:38:45 +0530

    Add files via upload

---
+++ Day3/ifThenElse/src/Main.java
@@ -0,0 +1,34 @@
+public class Main {
+    public static void main(String[] args) {
+
+        boolean gameOver = true;
+        int score = 800;
+        int levelCompleted = 5;
+        int bonus = 100;
+
+        if(gameOver){
+            int finalScore = score + (levelCompleted * bonus);
+            System.out.println("your final score was" + finalScore);
+        }
+
+        //boolean newGameOver = true;
+        //int secondScore = 10000;
+        //int secondLevelCompleted = 8;
+        //int secondBonus = 200;
+
+        //if(newGameOver){
+            //int finalScore = secondScore + (secondLevelCompleted * secondBonus);
+            //System.out.println("your final score was" + finalScore);
+        //}
+
+        score = 10000;
+        levelCompleted = 8;
+        bonus = 200;
+
+        if(gameOver){
+            int finalScore = score + (levelCompleted * bonus);
+            System.out.println("your final score was" + finalScore);
+        }
+
+    }
+}
